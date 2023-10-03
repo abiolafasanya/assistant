7ed3bc5f012cd869d27532fab8bb653ac76eaf40
abiolafasanya <hezekiahfasanya@gmail.com>
2023-10-03 10:30:37 +0100

    fix: removed duplicate import

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 
 window.addEventListener('load', function () {
   // Create Scene
